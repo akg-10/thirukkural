f0d29752b10ba75b94be954c5839a02925767546
akg-10 <akgv2002@gmail.com>
2024-06-01 16:30:30 +0530

    Added extra fields to the model

--- thirukkural-backend/kural/models.py
+++ thirukkural-backend/kural/models.py
@@ -3,9 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Thirukkural(models.Model):
-    number = models.IntegerField(unique=True) 
-    couplet = models.CharField(max_length=500)
-    meaning = models.TextField()
+    number = models.IntegerField(unique=True)
+    couplet_tamil = models.TextField()
+    meaning_tamil = models.TextField()
+    couplet_en = models.TextField()
+    meaning_en = models.TextField()
+    section = models.CharField(max_length=100, default='Unknown Section')
+    section_en = models.CharField(max_length=100, default='Unknown Section')
+    section_tang = models.CharField(max_length=100, default='Unknown Section')
+    chapter_group = models.CharField(max_length=100, default='Unknown Chapter Group')
+    chapter_group_en = models.CharField(max_length=100, default='Unknown Chapter Group')
+    chapter_group_tang = models.CharField(max_length=100, default='Unknown Chapter Group')
+    chapter = models.CharField(max_length=100, default='Unknown Chapter')
+    chapter_en = models.CharField(max_length=100, default='Unknown Chapter')
+    chapter_tang = models.CharField(max_length=100, default='Unknown Chapter')
 
     def __str__(self):
-        return f"{self.number}: {self.couplet}"
+        return f"{self.number}: {self.couplet_tamil}"
--- thirukkural-backend/kural/serializers.py
+++ thirukkural-backend/kural/serializers.py
@@ -4,4 +4,4 @@ from .models import Thirukkural
 class ThirukkuralSerializer(serializers.ModelSerializer):
     class Meta:
         model = Thirukkural
-        fields = ['number', 'couplet', 'meaning']
+        fields = ['number', 'couplet_tamil', 'meaning_tamil', 'couplet_en', 'meaning_en', 'section', 'section_en', 'section_tang', 'chapter_group', 'chapter_group_en', 'chapter_group_tang', 'chapter', 'chapter_en', 'chapter_tang']
