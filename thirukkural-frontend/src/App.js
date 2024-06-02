b9e787cafe1c9d78e52ad64157ef057c989f83b8
akg-10 <akgv2002@gmail.com>
2024-06-02 20:10:09 +0530

    Added Language toggle feature

--- thirukkural-frontend/src/App.js
+++ thirukkural-frontend/src/App.js
@@ -3,6 +3,26 @@ import axios from 'axios';
 
 function App() {
   const [kural, setKural] = useState(null);
+  const [lang, setLang] = useState(
+    {
+      en: false
+    }
+  );
+
+  function toggleLang(){
+    if (lang.en) {
+      setLang(
+        {
+          en: false
+        }
+      ) 
+    } else {
+      setLang({
+        en: true
+      })
+    }
+    
+  }
 
   useEffect(() => {
     axios.get('http://localhost:8000/api/random-kural/')
@@ -17,17 +37,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Thirukkural of the Day</h1>
+        <div>
+        <button onClick={toggleLang}>{lang.en ? "தமிழ்" : "English"}</button>
+        </div>
+        <h1>{lang.en ? "Thirukkural of the Day" : "இன்றைய திருக்குறள்"}</h1>
         {kural ? (
           <div>
-            <h2>Couplet {kural.number}</h2>
-            <p>{kural.couplet_tamil}</p>
-            <p><em>{kural.meaning_tamil}</em></p>
+            {lang.en ? (
+            <div>
+            <h2>Couplet: {kural.number}</h2>
             <p>{kural.couplet_en}</p>
             <p><em>{kural.meaning_en}</em></p>
+            </div>
+            ) : (
+            <div>
+            <h2>குறள்: {kural.number}</h2>
+            <p>{kural.couplet_tamil}</p>
+            <p><em>{kural.meaning_tamil}</em></p>
+            </div>
+            )} 
           </div>
         ) : (
-          <p>Loading...</p>
+          <p>{lang.en ? "Loading..." : "குறள் கிடைக்கும் வரை காத்திருங்கள்"}</p>
         )}
       </header>
     </div>
